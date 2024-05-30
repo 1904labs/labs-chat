47988c0e8f8ed9edcb2e0db7affc08b830608da4
Matt Scaperoth <scaperoth@gmail.com>
2024-05-30 15:21:51 -0500

    making todo notes for future possible changes

--- frontend/labs-chat/src/helpers/sessionId.js
+++ frontend/labs-chat/src/helpers/sessionId.js
@@ -1,4 +1,16 @@
+// we want to generate a random session id each 
+// time the react app is loaded and then cache that
+// value so we can return it in the export
+// this way, components can share the same session
+// 
+// POSSIBLE TODO: have the session managed in this file
+// use the localstorage instead so that we can 
+// pull from there and reuse the session without 
+// worrying about page refreshes
 
+// don't think we need to import crypto for this?
+// ... do we? Seems to just work.
 const sessionId = crypto.randomUUID();
 
+// export the session id as a "cached" value
 export default sessionId;
