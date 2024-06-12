19bef47110537f5686521345aa2802c7fcb1230d
NAPTlME <napratt2@yahoo.com>
2024-06-12 10:26:43 -0500

    Adding signed type to function parameters

--- frontend/labs-chat/src/helpers/memory.ts
+++ frontend/labs-chat/src/helpers/memory.ts
@@ -29,7 +29,7 @@ export class Memory {
         this.ai_role = "assistant";
     }
 
-     addHumanMessage(message): void {
+     addHumanMessage(message: string): void {
         // todo: add a limit to the context size, plus auto reduce context size
         if (this.verbose) {
             console.log(`Adding human message: ${message}`);
@@ -97,7 +97,7 @@ export class Memory {
         this.clearAIStream();
     };
 
-    accumulateAIStream(textDelta): void {
+    accumulateAIStream(textDelta: string): void {
         if (this.verbose) {
             console.log(`Accumulating AI stream: ${textDelta}`);
         }
