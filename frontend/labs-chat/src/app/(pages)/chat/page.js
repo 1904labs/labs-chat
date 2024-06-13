a7ca37a4d149a6a9941120b88fb7b88c13d5d629
Justin McCann <justin.mccann@1904labs.com>
2024-06-13 09:09:22 -0500

    Custom hook example

--- frontend/labs-chat/src/app/(pages)/chat/page.js
+++ frontend/labs-chat/src/app/(pages)/chat/page.js
@@ -1,37 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PromptBox from "../../components/PromptBox";
+import useChat from "../../hooks/useChat";
 
 export default function Chat() {
-  const [prompt, setPrompt] = useState("");
-  const [message, setMessage] = useState("DEFAULT");
-  const [firstMsg, setFirstMsg] = useState(true);
-
-  useEffect(() => {
-    if (prompt && prompt !== "") {
-      handleSubmitPrompt();
-    }
-  }, [prompt]);
-
-  const handleSubmitPrompt = async () => {
-    try {
-      // Send the prompt to the API
-      const response = await fetch("/api/home", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input: prompt, firstMsg }),
-      });
-      const responseJson = await response.json();
-      console.log(responseJson.output.response);
-      setMessage(responseJson.output.response);
-      setFirstMsg(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { message, setPrompt } = useChat();
 
   return (
     <div>
