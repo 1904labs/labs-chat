3fd48debe448c88cbea5b0403b4ab29fd7b0996b
Matt Scaperoth <scaperoth@gmail.com>
2024-06-19 15:44:14 -0500

    lint

--- frontend/labs-chat/src/app/components/AmplifyClientProvider.tsx
+++ frontend/labs-chat/src/app/components/AmplifyClientProvider.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import { Amplify } from "aws-amplify";
 import { authConfig } from "../amplify-cognito-config";
 
 export default function AmplifyClientProvider() {
-    Amplify.configure(
-        {
-            Auth: authConfig,
-        },
-        { ssr: true },
-    );
+  Amplify.configure(
+    {
+      Auth: authConfig,
+    },
+    { ssr: true },
+  );
 
-    return null;
+  return null;
 }
