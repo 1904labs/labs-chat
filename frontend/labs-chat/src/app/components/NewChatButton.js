8cfbb2ee1292c385d3bf974b2512f2b6c1c68fd4
NAPTlME <napratt2@yahoo.com>
2024-06-20 11:21:16 -0500

    Moved the NewChatButton in the sidebar to it's own component. Defined an onClick handler which will clear the server and client session history

---
+++ frontend/labs-chat/src/app/components/NewChatButton.js
@@ -0,0 +1,30 @@
+"use client"
+
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import { useChatHistory } from "@helpers/ClientChatHistory"
+
+const NewChatButton = () => {
+    const { clearMessageHistory } = useChatHistory();
+    const handleOnClick = async () => {
+        // clear the memory on the server side
+        const response = await fetch("/api/newChat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: "{}",
+        });
+        console.log(`newChat Response: ${JSON.stringify(response.json())}`);
+
+        // clear the client chat history
+        clearMessageHistory();
+    }
+    return (
+        <button onClick={handleOnClick} className="flex items-center justify-start bg-1904labs-green-500 p-4 hover:bg-1904labs-green-600">
+            <PlusCircleIcon className="mr-2 h-6 w-6 text-white drop-shadow-lg" />
+            <span className="text-white drop-shadow-lg">New Chat</span>
+        </button>
+    )
+}
+
+export default NewChatButton
