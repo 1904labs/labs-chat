2deed15d5059175481ce8bdd5ebd39ced2743d4c
NAPTlME <napratt2@yahoo.com>
2024-06-20 11:20:44 -0500

    Moved the client's chat history out of ChatStreamingWindow to a separate client helper component `ClientChatHistory` this holds the chat history and exposes a couple functions to add/clear history.

--- frontend/labs-chat/src/app/(pages)/layout.js
+++ frontend/labs-chat/src/app/(pages)/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import Navbar from "@components/Navbar";
 import Sidebar from "@components/Sidebar";
+import { ClientChatHistory } from "@helpers/ClientChatHistory"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({ children }) {
         <div className="flex h-screen flex-col">
           <Navbar />
           <main className="flex flex-grow flex-row overflow-y-auto">
-            <Sidebar />
-            <div className="flex flex-1 bg-[url('/assets/office_candid_background.jpeg')] bg-cover bg-[center_top] bg-no-repeat">
-              {children}
-            </div>
+            <ClientChatHistory>
+              <Sidebar />
+              <div className="flex flex-1 bg-[url('/assets/office_candid_background.jpeg')] bg-cover bg-[center_top] bg-no-repeat">
+                {children}
+              </div>
+            </ClientChatHistory>
           </main>
         </div>
       </body>
---
+++ frontend/labs-chat/src/helpers/ClientChatHistory.js
@@ -0,0 +1,38 @@
+"use client"
+import { createContext, useContext, useState } from 'react';
+import { getFormattedDateForUI } from "@helpers/dates";
+
+const ChatContext = createContext();
+
+export const useChatHistory = () => useContext(ChatContext);
+
+function makeDefaultMessage() {
+    return {
+      id: 0,
+      speaker: "bot",
+      message: "Hello! How can I help you today?",
+      date: getFormattedDateForUI(),
+    };
+  }
+
+export const ClientChatHistory = ({ children }) => {
+    const [chatHistory, setChatHistory] = useState([makeDefaultMessage()]);
+
+    const clearMessageHistory = () => {
+        setChatHistory([makeDefaultMessage()]);
+    };
+
+    const addMessageToHistory = (message, callback) => {
+        if (callback) {
+          setChatHistory((prevMessages) => [...prevMessages, message], callback);
+        } else {
+          setChatHistory((prevMessages) => [...prevMessages, message]);
+        }
+      };
+
+    return (
+        <ChatContext.Provider value={{ chatHistory, clearMessageHistory, addMessageToHistory }}>
+            {children}
+        </ChatContext.Provider>
+    );
+};
