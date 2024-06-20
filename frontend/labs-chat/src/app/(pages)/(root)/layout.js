490ce8f4b5005d6f81988d4a841cd1cba375581e
NAPTlME <napratt2@yahoo.com>
2024-06-20 11:35:12 -0500

    Adding ClientChatHistory to the new layout for the sidebar to use (for new chat button)

--- frontend/labs-chat/src/app/(pages)/(root)/layout.js
+++ frontend/labs-chat/src/app/(pages)/(root)/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "@components/Navbar";
 import Sidebar from "@components/Sidebar";
+import { ClientChatHistory } from "@helpers/ClientChatHistory"
 
 export const metadata = {
   title: "Labs Chat",
@@ -11,10 +12,12 @@ export default function PagesLayout({ children }) {
     <div className="flex h-screen flex-col">
       <Navbar />
       <main className="flex flex-grow flex-row overflow-y-auto">
-        <Sidebar />
-        <div className="flex flex-1 bg-[url('/assets/office_candid_background.jpeg')] bg-cover bg-[center_top] bg-no-repeat">
-          {children}
-        </div>
+        <ClientChatHistory>
+          <Sidebar />
+          <div className="flex flex-1 bg-[url('/assets/office_candid_background.jpeg')] bg-cover bg-[center_top] bg-no-repeat">
+            {children}
+          </div>
+        </ClientChatHistory>
       </main>
     </div>
   );
