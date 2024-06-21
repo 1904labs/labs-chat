a4502e66d44dba4f2232aa74260fc169de1ff73c
Matt Scaperoth <scaperoth@gmail.com>
2024-06-21 10:01:30 -0500

    updating lintstagedrc and package to only pretty staged files

--- frontend/labs-chat/.lintstagedrc.js
+++ frontend/labs-chat/.lintstagedrc.js
@@ -5,6 +5,14 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
+const runPrettierCommand = (filenames) =>
+  `npx prettier ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")} --write `;
+
+const addChanges = (filenames) =>
+  `git add ${filenames.map((f) => f).join(" ")}`;
+
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, runPrettierCommand, addChanges],
 };
