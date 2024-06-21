e48cbcfde0c411c5b4c335ec90c3e16b68052d01
NAPTlME <napratt2@yahoo.com>
2024-06-21 10:07:41 -0500

    Getting user_id for new chat.

--- frontend/labs-chat/src/app/api/newChat/route.ts
+++ frontend/labs-chat/src/app/api/newChat/route.ts
@@ -1,14 +1,21 @@
+import { cookies } from "next/headers";
+import { getCurrentUser } from "aws-amplify/auth/server";
 import { newChat } from "@/app/api/streaming-llm/route";
+import { runWithAmplifyServerContext } from "@helpers/amplify-server-utils";
 
 export async function POST(req: Request) {
-  try {
-    const request = await req.json();
-    await newChat("test_user");
-    return Response.json({ message: "Success" }, { status: 200 });
-  } catch (error) {
-    return Response.json(
-      { message: `Internal Server Error: ${error.message}` },
-      { status: 500 },
-    );
-  }
+    const user = await runWithAmplifyServerContext({
+        nextServerContext: { cookies },
+        operation: (contextSpec) => getCurrentUser(contextSpec),
+      });
+    try {
+        const request = await req.json();
+        await newChat(user.userId);
+        return Response.json({ message: "Success" }, { status: 200 });
+    } catch (error) {
+        return Response.json(
+            { message: `Internal Server Error: ${error.message}` },
+            { status: 500 },
+        );
+    }
 }
