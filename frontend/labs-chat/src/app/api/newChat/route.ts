d07b2da9c5fc11e739d1deb42d4c9c91edd4a90e
NAPTlME <napratt2@yahoo.com>
2024-06-24 10:37:29 -0500

    Using the `authenticatedUser` function to get the user

--- frontend/labs-chat/src/app/api/newChat/route.ts
+++ frontend/labs-chat/src/app/api/newChat/route.ts
@@ -1,14 +1,11 @@
 import { cookies } from "next/headers";
 import { getCurrentUser } from "aws-amplify/auth/server";
 import { newChat } from "@/app/api/streaming-llm/route";
-import { runWithAmplifyServerContext } from "@helpers/amplify-server-utils";
+import { authenticatedUser } from "@helpers/amplify-server-utils";
 
 export async function POST(req: Request) {
   // todo: consolidate where this code lives
-  const user = await runWithAmplifyServerContext({
-    nextServerContext: { cookies },
-    operation: (contextSpec) => getCurrentUser(contextSpec),
-  });
+  const user = await authenticatedUser({ cookies });
   try {
     const request = await req.json();
     await newChat(user.userId);
