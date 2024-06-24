54bdc5412273171a739ad7906d2bb4435330fffe
Jocelyn Kaye Matsuo <jocelyn.matsuo@gmail.com>
2024-06-24 16:29:47 -0500

    remove unused method

--- frontend/labs-chat/src/app/api/userAttributes/route.ts
+++ frontend/labs-chat/src/app/api/userAttributes/route.ts
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import { runWithAmplifyServerContext } from "@helpers/amplify-server-utils";
-import { getCurrentUser } from "aws-amplify/auth/server";
 import { fetchUserAttributes } from "aws-amplify/auth/server";
 
 export async function GET(): Promise<Response> {
