97b53bb8a46803a2a4999c408e9c9f9dca03a91f
Jocelyn Kaye Matsuo <jocelyn.matsuo@gmail.com>
2024-06-24 16:31:59 -0500

    Add some comments

--- frontend/labs-chat/src/app/api/userAttributes/route.ts
+++ frontend/labs-chat/src/app/api/userAttributes/route.ts
@@ -3,6 +3,8 @@ import { runWithAmplifyServerContext } from "@helpers/amplify-server-utils";
 import { fetchUserAttributes } from "aws-amplify/auth/server";
 
 export async function GET(): Promise<Response> {
+  // This method will return the user attributes of the current user
+  // { email: string, sub: string, name?: string }
   const attributes = await runWithAmplifyServerContext({
     nextServerContext: { cookies },
     operation: (contextSpec) => fetchUserAttributes(contextSpec),
