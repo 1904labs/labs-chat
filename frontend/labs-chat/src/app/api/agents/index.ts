19a6688e93f9de1bb35fe0a1396f96bfd07ecfcd
NAPTlME <napratt2@yahoo.com>
2024-06-28 13:04:58 -0500

    Moving agents files to match the api route format

--- frontend/labs-chat/src/app/api/agents/index.ts
+++ frontend/labs-chat/src/app/api/agents/index.ts
@@ -1,3 +1,3 @@
-export { default as DEFAULT_CHAT_AGENT } from "./default_chat";
-export { default as POLICY_RETRIEVAL_AGENT } from "./policy_retrieval_kb";
-export { default as ROUTING_AGENT } from "./routing";
+export { default as DEFAULT_CHAT_AGENT } from "./defaultChat/route";
+export { default as POLICY_RETRIEVAL_AGENT } from "./policyRetrieval/route";
+export { default as ROUTING_AGENT } from "./routing/route";
